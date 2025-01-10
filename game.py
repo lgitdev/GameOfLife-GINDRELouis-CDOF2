587b36d488a505725ea0cf69eaacd9eca534cfd9
Louis GINDRE <louis.gindre@edu.devinci.fr>
2025-01-10 20:08:48 +0100

    Initialize cells with mouse

--- game.py
+++ game.py
@@ -1,8 +1,13 @@
 import pygame
+from pygame.locals import *
 
 # color constants
 BLACK = (0, 0, 0)
 WHITE = (200, 200, 200)
+
+# values of the cells
+cells = [[0] * 64 for i in range(39)]
+cellSize = 20 
 
 def main():
     global screen, clock # let these parameters accessible everywhere so we can access these variables in every method
@@ -10,14 +15,17 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((1280, 720))
     clock = pygame.time.Clock()
-    screen.fill(BLACK)
+    screen.fill(WHITE)
     running = True
 
     while running:
         drawGrid() # create a grid for the game
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == MOUSEBUTTONDOWN:
+                updateGrid(event)
 
         pygame.display.update()
     
@@ -26,12 +34,19 @@ def main():
 
 
 def drawGrid():
-    cellSize = 20 # size of a cell
     for x in range(0, 1280, cellSize): # create the grid
         for y in range(0, 780, cellSize):
             rect = pygame.Rect(x, y, cellSize, cellSize)
-            pygame.draw.rect(screen, WHITE, rect, 1)
+            if cells[y//20][x//20] == 0: # cell dead
+                pygame.draw.rect(screen, BLACK, rect, 10)
+            else: # cell alive
+                pygame.draw.rect(screen, WHITE, rect, 10)
 
+def updateGrid(event):
+    x, y = event.pos # from the mouse click, change if a cell is alive or not
+    x //= 20
+    y //= 20
+    cells[y][x] = 1 - cells[y][x]
 
 
 main()
